757a8bd2129d995f7bb3a650fe5edfaafc82b274
jlbuild <juliabuildbot@gmail.com>
2020-10-24 21:47:10 +0000

    GLPK_jll build 4.65.0+1

--- src/wrappers/i686-w64-mingw32.jl
+++ src/wrappers/i686-w64-mingw32.jl
@@ -2,57 +2,15 @@
 export libglpk
 
 using GMP_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "PATH"
-
-# Relative path to `libglpk`
-const libglpk_splitpath = ["bin", "libglpk-40.dll"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libglpk_path = ""
-
-# libglpk-specific global declaration
-# This will be filled out by __init__()
-libglpk_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libglpk = "libglpk-40.dll"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("GLPK")
+JLLWrappers.@declare_library_product(libglpk, "libglpk-40.dll")
 function __init__()
-    global artifact_dir = abspath(artifact"GLPK")
+    JLLWrappers.@generate_init_header(GMP_jll)
+    JLLWrappers.@init_library_product(
+        libglpk,
+        "bin\\libglpk-40.dll",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # We first need to add to LIBPATH_list the libraries provided by Julia
-    append!(LIBPATH_list, [Sys.BINDIR, joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (GMP_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (GMP_jll.LIBPATH_list,))
-
-    global libglpk_path = normpath(joinpath(artifact_dir, libglpk_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libglpk_handle = dlopen(libglpk_path)
-    push!(LIBPATH_list, dirname(libglpk_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ';')
-    global LIBPATH = join(LIBPATH_list, ';')
-
-    # Add each element of LIBPATH to our DL_LOAD_PATH (necessary on platforms
-    # that don't honor our "already opened" trick)
-    #for lp in LIBPATH_list
-    #    push!(DL_LOAD_PATH, lp)
-    #end
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
